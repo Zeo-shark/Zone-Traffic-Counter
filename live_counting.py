47f7571e01dbdf716d67063df83b01ab4e70efb5
Zeo-shark <sauravberaiit@gmail.com>
2021-02-22 16:04:39 +0530

    add devices specific computational devices gpu or cpu

--- live_counting.py
+++ live_counting.py
@@ -6,3 +6,8 @@ import argparse
 import cv2
 import copy
 import os
+
+def main_live(args):
+    capture= cv2.VideoCapture(args.stream)
+    num_gpus= torch.cuda.device_count()
+    device = 'cuda' if num_gpus >= 1 else 'cpu'
