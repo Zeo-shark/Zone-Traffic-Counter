5ea8ffcffaefeba0298ede5e8029a06ca59cfaa0
Zeo-shark <sauravberaiit@gmail.com>
2021-02-22 16:11:52 +0530

    load model CANNet

--- live_counting.py
+++ live_counting.py
@@ -11,3 +11,5 @@ def main_live(args):
     capture= cv2.VideoCapture(args.stream)
     num_gpus= torch.cuda.device_count()
     device = 'cuda' if num_gpus >= 1 else 'cpu'
+    model= CANNet().to(device)
+    model.load_state_dict(torch.load(args.weights,map_location=torch.device(device)))
